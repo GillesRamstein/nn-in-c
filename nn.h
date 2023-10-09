8d521c43c3ae17268978b809cd397f88468c2e64
--show-scope <--show>
2023-10-09 18:23:09 +0200

    add mat_row, mat_copy to nn.h; fix mat_mul_mat; add more tests

--- nn.h
+++ nn.h
@@ -8,6 +8,7 @@
 
 #include <stddef.h> // size_t
 #include <stdio.h> // printf
+#include <math.h> // expf
 
 #ifndef NN_MALLOC
 #include <stdlib.h>
@@ -22,22 +23,29 @@
 typedef struct {
   size_t num_rows;
   size_t num_cols;
+  size_t stride;
   float *p_data;
 } Matrix;
 
-#define MAT_AT(mat, row, col) mat.p_data[(row)*(mat).num_cols + (col)]
+#define MAT_AT(mat, row, col) mat.p_data[(row)*(mat).stride + (col)]
+
+float rand_float();
+float sigmoid(float x);
 
 Matrix mat_alloc(size_t num_rows, size_t num_cols);
 void mat_print(Matrix m, const char *name);
 #define MAT_PRINT(m) mat_print(m, #m);
-void mat_fill(Matrix m, float val);
-float rand_float();
+
+void mat_fill(Matrix m, float x);
 void mat_rand(Matrix m, float min, float max);
-void mat_trp(Matrix m);
-void mat_add_num(Matrix a, float b);
+Matrix mat_row(Matrix m, size_t row);
+void mat_copy(Matrix dst, Matrix m);
+// void mat_trp(Matrix m);
+void mat_add_num(Matrix m, float x);
 void mat_add_mat(Matrix a, Matrix b);
-void mat_mul_num(Matrix a, float b);
+void mat_mul_num(Matrix m, float x);
 void mat_mul_mat(Matrix dst, Matrix a, Matrix b);
+void mat_sigmoid(Matrix m);
 
 #endif // NN_H
 
@@ -49,10 +57,19 @@ void mat_mul_mat(Matrix dst, Matrix a, Matrix b);
 
 #ifdef NN_IMPLEMENTATION
 
+float rand_float(void) {
+  return (float) rand() / (float) RAND_MAX;
+}
+
+float sigmoidf(float x) {
+  return 1.f / (1.f + expf(-x));
+}
+
 Matrix mat_alloc(size_t num_rows, size_t num_cols) {
   Matrix m;
   m.num_rows = num_rows;
   m.num_cols = num_cols;
+  m.stride = num_cols;
   m.p_data = NN_MALLOC(sizeof(*m.p_data)*num_rows*num_cols);
   NN_ASSERT(m.p_data != NULL);
   return m;
@@ -61,25 +78,21 @@ Matrix mat_alloc(size_t num_rows, size_t num_cols) {
 void mat_print(Matrix m, const char *name) {
   printf("%s = [\n", name);
   for (size_t row=0; row<m.num_rows; ++row) {
-    printf("  ");
+    printf(" [ ");
     for (size_t col=0; col<m.num_cols; ++col) {
       printf("%f  ", MAT_AT(m, row, col));
     }
-    printf("\n");
+    printf(" ]\n");
   }
     printf("]\n");
 }
 
-void mat_fill(Matrix m, float val) {
+void mat_fill(Matrix m, float x) {
   for (size_t row=0; row<m.num_rows; ++row) {
     for (size_t col=0; col<m.num_cols; ++col) {
-      MAT_AT(m, row, col) = val;
+      MAT_AT(m, row, col) = x;
     }
   }
-}
-
-float rand_float(void) {
-  return (float) rand() / (float) RAND_MAX;
 }
 
 void mat_rand(Matrix m, float min, float max) {
@@ -90,10 +103,29 @@ void mat_rand(Matrix m, float min, float max) {
   }
 }
 
-void mat_add_num(Matrix a, float b) {
-  for (size_t row=0; row<a.num_rows; ++row) {
-    for (size_t col=0; col<a.num_cols; ++col) {
-      MAT_AT(a, row, col) += b;
+Matrix mat_row(Matrix m, size_t row) {
+  return (Matrix){
+    .num_rows = 1,
+    .num_cols = m.num_cols,
+    .stride = m.stride,
+    .p_data = &MAT_AT(m, row, 0),
+  };
+}
+
+void mat_copy(Matrix dst, Matrix m) {
+  NN_ASSERT(dst.num_rows == m.num_rows);
+  NN_ASSERT(dst.num_cols == m.num_cols);
+  for (size_t row=0; row<m.num_rows; ++row) {
+    for (size_t col=0; col<m.num_cols; ++col) {
+      MAT_AT(dst, row, col) = MAT_AT(m, row, col);
+    }
+  }
+}
+
+void mat_add_num(Matrix m, float x) {
+  for (size_t row=0; row<m.num_rows; ++row) {
+    for (size_t col=0; col<m.num_cols; ++col) {
+      MAT_AT(m, row, col) += x;
     }
   }
 }
@@ -108,25 +140,34 @@ void mat_add_mat(Matrix a, Matrix b) {
   }
 }
 
-void mat_mul_num(Matrix a, float b) {
-  for (size_t row=0; row<a.num_rows; ++row) {
-    for (size_t col=0; col<a.num_cols; ++col) {
-      MAT_AT(a, row, col) *= b;
+void mat_mul_num(Matrix m, float x) {
+  for (size_t row=0; row<m.num_rows; ++row) {
+    for (size_t col=0; col<m.num_cols; ++col) {
+      MAT_AT(m, row, col) *= x;
     }
   }
 }
 
 void mat_mul_mat(Matrix dst, Matrix a, Matrix b) {
   assert(a.num_cols == b.num_rows);
+  size_t inner_dim = a.num_cols;
+
   assert(dst.num_rows == a.num_rows);
   assert(dst.num_cols == b.num_cols);
   for (size_t row=0; row<dst.num_rows; ++row) {
     for (size_t col=0; col<dst.num_cols; ++col) {
-      float tmp = 0;
-      for (size_t i=0; i<dst.num_cols; ++i) {
-        tmp += MAT_AT(a, row, i) * MAT_AT(b, i, col);
+      MAT_AT(dst, row, col) = 0;
+      for (size_t i=0; i<inner_dim; ++i) {
+        MAT_AT(dst, row, col) += (MAT_AT(a, row, i) * MAT_AT(b, i, col));
       }
-      MAT_AT(dst, row, col) = tmp;
+    }
+  }
+}
+
+void mat_sigmoid(Matrix m) {
+  for (size_t row=0; row<m.num_rows; ++row) {
+    for (size_t col=0; col<m.num_cols; ++col) {
+      MAT_AT(m, row, col)=sigmoidf(MAT_AT(m, row, col));
     }
   }
 }
--- test_nn.c
+++ test_nn.c
@@ -1,41 +1,112 @@
 #define NN_IMPLEMENTATION
 #include "nn.h"
 
+void test_mat_mul_mat_1() {
+  /* [[ 7 10 ] [ 15 22 ]] */
+  printf("------------------------------\n");
+  printf("Mat mul 2x2 * 2x2\n");
+  Matrix m1 = mat_alloc(2, 2);
+  Matrix m2 = mat_alloc(2, 2);
+  float e1[4] = { 1, 2, 3, 4 };
+  float e2[4] = { 1, 2, 3, 4 };
+  m1.p_data = e1;
+  m2.p_data = e2;
+  MAT_PRINT(m1);
+  MAT_PRINT(m2);
+  Matrix m1xm2 = mat_alloc(2, 2);
+  mat_mul_mat(m1xm2, m1, m2);
+  MAT_PRINT(m1xm2);
+  printf("\n");
+}
+
+void test_mat_mul_mat_2() {
+  /* [[ 6 ] [ 8 ]] */
+  printf("Mat mul 2x1 * 1x1\n");
+  printf("------------------------------\n");
+  Matrix m1 = mat_alloc(2, 1);
+  Matrix m2 = mat_alloc(1, 1);
+  float e1[2] = { 3, 4 };
+  float e2[1] = { 2 };
+  m1.p_data = e1;
+  m2.p_data = e2;
+  MAT_PRINT(m1);
+  MAT_PRINT(m2);
+  Matrix m1xm2 = mat_alloc(2, 1);
+  mat_mul_mat(m1xm2, m1, m2);
+  MAT_PRINT(m1xm2);
+  printf("\n");
+}
+
+void test_mat_mul_mat_3() {
+  /* [ 6 8 ] */
+  printf("------------------------------\n");
+  printf("Mat mul 1x1 * 1x2\n");
+  Matrix m1 = mat_alloc(1, 1);
+  Matrix m2 = mat_alloc(1, 2);
+  float e1[1] = { 2 };
+  float e2[2] = { 3, 4 };
+  m1.p_data = e1;
+  m2.p_data = e2;
+  MAT_PRINT(m1);
+  MAT_PRINT(m2);
+  Matrix m1xm2 = mat_alloc(1, 2);
+  mat_mul_mat(m1xm2, m1, m2);
+  MAT_PRINT(m1xm2);
+  printf("\n");
+}
+
+void test_mat_mul_mat_4() {
+  /* [[ 11 14 17 20 ] [23 30 37 44 ] [ 35 46 57 68 ]] */
+  printf("------------------------------\n");
+  printf("Mat mul 3x2 * 2x4\n");
+  Matrix m1 = mat_alloc(3, 2);
+  Matrix m2 = mat_alloc(2, 4);
+  float e1[6] = { 1, 2, 3, 4, 5, 6};
+  float e2[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+  m1.p_data = e1;
+  m2.p_data = e2;
+  MAT_PRINT(m1);
+  MAT_PRINT(m2);
+  Matrix m1xm2 = mat_alloc(3, 4);
+  mat_mul_mat(m1xm2, m1, m2);
+  MAT_PRINT(m1xm2);
+  printf("\n");
+}
 
 int main(void) {
 
   srand(1);
 
-  // alloc 1x1; fill; print
+  // alloc 1x1;fill;print
   Matrix m_1_1 = mat_alloc(1, 1);
   mat_fill(m_1_1, -1);
   MAT_PRINT(m_1_1);
-  printf("\n"); // ---------------------------------------------
-
-  // alloc 2x3; rand
+  printf("\n");
+  
+  // alloc 2x3;rand
   Matrix m_2_3 = mat_alloc(2, 3);
   mat_rand(m_2_3, 0, 1);
   MAT_PRINT(m_2_3);
-  printf("\n"); // ---------------------------------------------
-
-  // alloc 4x3; add mat; add num; mul num
+  printf("\n");
+  
+  // alloc 4x3;add mat;add num;mul num
   Matrix m_4_3 = mat_alloc(4, 3);
   mat_rand(m_4_3, 0, 1);
   MAT_PRINT(m_4_3);
-  printf("\n"); // ---------------------------------------------
-
+  printf("\n");
+  
   mat_add_mat(m_4_3, m_4_3);
   MAT_PRINT(m_4_3);
-  printf("\n"); // ---------------------------------------------
-
+  printf("\n");
+  
   mat_add_num(m_4_3, 17);
   MAT_PRINT(m_4_3);
-  printf("\n"); // ---------------------------------------------
-
+  printf("\n");
+  
   mat_mul_num(m_4_3, 10);
   MAT_PRINT(m_4_3);
-  printf("\n"); // ---------------------------------------------
-
+  printf("\n");
+  
   // mul_mat
   Matrix dst = mat_alloc(3, 4);
   mat_fill(dst, 0);
@@ -48,8 +119,8 @@ int main(void) {
   MAT_PRINT(b);
   mat_mul_mat(dst, a, b);
   MAT_PRINT(dst);
-  printf("\n"); // ---------------------------------------------
-
+  printf("\n");
+  
   // mul_mat
   Matrix dst1 = mat_alloc(1, 1);
   mat_fill(dst1, 0);
@@ -62,10 +133,14 @@ int main(void) {
   MAT_PRINT(b);
   mat_mul_mat(dst1, a1, b1);
   MAT_PRINT(dst1);
-  printf("\n"); // ---------------------------------------------
+  printf("\n");
   
+  test_mat_mul_mat_1();
+  test_mat_mul_mat_2();
+  test_mat_mul_mat_3();
+  test_mat_mul_mat_4();
+
   printf("> finished all tests\n");
-  NN_ASSERT(0);
 
   // TODO: free memory?
 }
