c173eac986b244d96480ca84d724645297ddfa8d
--show-scope <--show>
2023-10-19 21:05:34 +0200

    rename and move files to dir

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 mkdir -p build
 
-# gcc timestwo.c -o build/timestwo -O0 -g
-# gcc logicgates.c -o build/logicgates -O0 -g -lm
-# gcc logicgates_xor.c -o build/logicgates_xor -O0 -g -lm
+# gcc witout_nn/timestwo.c -o build/timestwo -O0 -g
+# gcc witout_nn/logicgates.c -o build/logicgates -O0 -g -lm
+# gcc witout_nn/logicgates_xor.c -o build/logicgates_xor -O0 -g -lm
 gcc logicgates_xor_nn.c -o build/logicgates_xor_nn -O0 -g -Wall -Wextra -lm
 
-gcc test_nn.c -o build/test_nn -O0 -g -Wall -Wextra -lm
+# gcc test_nn_mat.c -o build/test_nn_mat -O0 -g -Wall -Wextra -lm
 
 
 if [[ -n $1 ]] && [[ "${1}" = "run" ]]
@@ -20,5 +20,5 @@ fi
 
 if [[ -n $1 ]] && [[ "${1}" = "test" ]]
 then
-  build/test_nn
+  build/test_nn_mat
 fi
